c526843fbe333f867fe61341791b80969252c894
Linus Samuelsson <smulle.junior@gmail.com>
2025-12-16 10:01:53 +0100

    Adds loan
    Co-authored-by: Dennis Selden <111012436+dennsel@users.noreply.github.com>
    
    Co-authored-by: Linda Eskilsson
    <eskilsson.linda@hotmail.se>
    
    Co-authored-by: Erika Falk Svendsen
    <alicia-fs@outlook.com>

---
+++ src/main/java/org/example/Loan.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+@Entity
+public class Loan {
+
+    @Id
+    private Long loanId;
+
+    private ZonedDateTime loanDate;
+    private ZonedDateTime returnDate;
+    private Long userId;
+    private Long bookId;
+
+    public Loan(){
+    }
+
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public ZonedDateTime getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(ZonedDateTime returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public ZonedDateTime getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(ZonedDateTime loanDate) {
+        this.loanDate = loanDate;
+    }
+}
