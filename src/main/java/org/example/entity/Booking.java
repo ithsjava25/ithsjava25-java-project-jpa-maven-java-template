556f0c0c03d6a5781df03f9cb6da276bed2fb2ae
Samuel Fjellet <229708690+Samuel-Fjellet@users.noreply.github.com>
2025-12-16 11:45:57 +0100

    Table genesis

--- src/main/java/org/example/entity/Booking.java
+++ src/main/java/org/example/entity/Booking.java
@@ -1,4 +1,48 @@
 package org.example.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="bookings")
+
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+//    @JoinColumn(name="table_id", nullable = false)
+    private BookingTable tableId;
+
+//    @ManyToMany(mappedBy = "guest")
+//    @JoinTable(name="guest_id",
+//        joinColumns = @JoinColumn(name = "booking_id"),
+//        inverseJoinColumns = @JoinColumn(name= "guest_id"))
+//    private Guest guestId;
+
+    @Column(name="Booking_Time", nullable = false)
+    private LocalDateTime time;
+
+    @Column(name="Party", nullable = false)
+    private int party;
+
+    public Booking(int tableId, LocalDateTime time, int party){
+        //this.tableId = tableId;
+        this.time = time;
+        this. party = party;
+    }
+
+    public Booking() {}
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
 }
--- src/main/java/org/example/entity/BookingTable.java
+++ src/main/java/org/example/entity/BookingTable.java
@@ -1,4 +1,35 @@
 package org.example.entity;
 
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "tables")
+
 public class BookingTable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="table_number", nullable = false, unique = true)
+    private int tableNumber;
+
+    @Column(name="Capacity", nullable = false)
+    private int capacity;
+
+    public BookingTable( int capacity, int tableNumber){
+        this.capacity = capacity;
+        this.tableNumber = tableNumber;
+    }
+
+    public BookingTable() {}
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
 }
