25951ac6a349c4127457313c21c31646759d34da
Tatjana <malina91983@gmail.com>
2025-12-18 21:04:05 +0100

    Changes App.java with JPAUtil

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,13 @@
 package org.example;
 
+import org.example.util.JPAUtil;
+
 public class App {
 
     static void main(String[] args) {
 
-
+        JPAUtil.inTransaction(em -> {
+            System.out.println("Database schema initialized");
+        });
     }
 }
