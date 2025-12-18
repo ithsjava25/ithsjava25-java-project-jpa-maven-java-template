720d615eeb4b3eeea7eba8b126933a5b3fa078f5
Tatjana <malina91983@gmail.com>
2025-12-18 20:42:09 +0100

    Moves EntityManagerFactory from App.java to JPAUtil

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,14 +1,9 @@
 package org.example;
 
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
-
 public class App {
 
     static void main(String[] args) {
-        EntityManagerFactory emf =
-            Persistence.createEntityManagerFactory("myPU");
 
-        emf.close();
+
     }
 }
