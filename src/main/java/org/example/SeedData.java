7d802379f0d44a3805d2bfb886c08c46869e8d0c
Linda Eskilsson <eskilsson.linda@hotmail.se>
2026-01-08 09:32:28 +0100

    Connect SeedData to persistence unit

--- src/main/java/org/example/SeedData.java
+++ src/main/java/org/example/SeedData.java
@@ -4,13 +4,20 @@ package org.example;
 //1. läsa TSV-fil: seed-books.tsv
 //2. skapa Author, Book, Genre-objekt
 //3. koppla dem
-//4. köra persist() + commit() → då hamnar datan i våran DB.
+//4. köra persist() + commit() → då hamnar datan i våran DB.f
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class SeedData {
-
     public static void main(String[] args) {
-        System.out.println("Seed startar...");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("library_system");
+        EntityManager em = emf.createEntityManager();
 
+        System.out.println("JPA ok ✅");
 
+        em.close();
+        emf.close();
     }
 }
