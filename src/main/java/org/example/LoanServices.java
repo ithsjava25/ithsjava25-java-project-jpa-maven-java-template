fcc8dd753c66bfd3a92d3a5ed1a1be1f2eaa9cf8
Erika Falk Svensen <alicia-fs@outlook.com>
2026-01-11 19:06:08 +0100

    Add isBookLoaned method

--- src/main/java/org/example/LoanServices.java
+++ src/main/java/org/example/LoanServices.java
@@ -1,4 +1,29 @@
 package org.example;
 
+import jakarta.persistence.EntityManager;
+import java.util.List;
+
 public class LoanServices {
+
+    final private EntityManager em;
+
+    public LoanServices(EntityManager em) {
+        this.em = em;
+    }
+
+    public boolean isBookLoaned(Long bookId) {
+
+        List<Loan> loans = em.createQuery(
+            "SELECT l FROM Loan l WHERE l.bookId = :bookId AND l.returnDate IS NULL",
+            Loan.class
+        )
+        .setParameter("bookId", bookId)
+            .getResultList();
+
+        if (loans.isEmpty()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
